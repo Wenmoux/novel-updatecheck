5ea11e1b0a90a03479226636989bab5b4f10c51e
wenmoux <wenmoux@gmail.com.com>
2020-12-29 10:06:12 +0000

    有更新啦

---
+++ chapter.md
@@ -0,0 +1,1 @@
+[第879章 禁断峡谷之乱（求订阅）](http://vipreader.qidian.com/chapter/1018027842/626330650)
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 config={
   id:1018027842,
-  cid:626077450,
+  cid:626330650,
   chaptername:"第879章 禁断峡谷之乱（求订阅）"
 }
 module.exports=config
